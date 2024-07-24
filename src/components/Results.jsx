612f30b0dd39523aee622b915d2f9ba607a09724
Fiewor <johnfiewor@gmail.com>
2024-07-24 22:56:54 +0100

    refactor: result

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { useMutationState, useMutation } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import {
@@ -14,116 +13,109 @@ import {
 import axios from "axios";
 import toast from "react-hot-toast";
 import { BASE_URL } from "../requests/constants";
+import { useMemo } from "react";
 
 const Results = () => {
   const nav = useNavigate();
-  const [sheetsUri, setSheetsUri] = useState("");
-
   const code = localStorage.getItem("code");
 
-  const {
-    isFetching: exportIsFetching,
-    isLoading: exportIsLoading,
-    isError: exportIsError,
-    isSuccess: exportIsSuccess,
-    error: exportError,
-    data: exportData,
-    mutate: exportMutate,
-  } = useMutation({
-    mutationKey: ["gradeData"],
-    mutationFn: async ({ code, sheetsObject }) => {
-      try {
-        const res = await axios.post(`${BASE_URL}/api/oauth2callback`, {
-          code,
-          sheetsObject,
-        });
-        console.log("res: ", res);
-        return res;
-      } catch (error) {
-        console.log(error);
-      }
+  const { isFetching, isLoading, isError, isSuccess, error, data, mutate } =
+    useMutation({
+      mutationKey: ["exportData"],
+      mutationFn: async ({ code, sheetsObject }) => {
+        try {
+          return await axios.post(`${BASE_URL}/api/oauth2callback`, {
+            code,
+            sheetsObject,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      // retry: 3,
+    });
+
+  const gradeData = useMutationState({
+    filters: "gradeData",
+    select: (mutation) => {
+      let data = mutation.state?.data?.data;
+      if (data) return data;
     },
-    // retry: 3,
   });
 
-  // const data = useMutationState({
-  //   filters: "gradeData",
-  //   select: (mutation) => {
-  //     let data = mutation.state?.data?.data;
-  //     if (data) return data;
-  //   },
-  // });
+  let buttonText = "Export to Google Sheets.";
+  let gradingResponse_ = useMemo(() => {
+    if (gradeData?.length) {
+      const filteredGradeData = gradeData.filter((el) => el);
+      const filteredGradingResponse =
+        filteredGradeData[0]?.data?.gradingResponse?.filter((el) => el);
+      return filteredGradingResponse?.map((response) => JSON.parse(response));
+    } else {
+      return [];
+    }
+  }, [gradeData]);
 
-  let _gradingResponse = [{ score: "17/60" }, { score: "35/60" }]; //! test data to be used till new Gemini resource is created
-  // let _gradingResponse = [];
-  // console.log("data: ", data);
-  // if (data?.length) {
+  // let gradingResponse_ = [];
+  // console.log("gradeData: ", gradeData);
+  // if (gradeData?.length) {
+  //   const filteredGradeData = gradeData.filter((el) => el);
+  //   console.log("filteredGradeData: ", filteredGradeData);
   //   const {
   //     data: { gradingResponse }, // gradingResponse
-  //   } = data[0];
+  //   } = filteredGradeData[0];
 
-  //   _gradingResponse = gradingResponse;
+  //   gradingResponse_ = gradingResponse?.map(({ value }) => JSON.parse(value));
   // }
 
-  let buttonText = "Export to Google Sheets.";
-
-  useEffect(() => {
-    if (exportIsFetching || exportIsLoading) buttonText = "Exporting...";
-    if (exportIsError) {
-      console.log("error: ", exportError);
-      toast.error("An error occurred");
-      buttonText = "Retry";
-    }
-    if (exportIsSuccess) {
-      toast.success("Successfully exported data");
-      console.log("data: ", exportData);
-      setSheetsUri(exportData?.data?.data?.spreadsheetUrl);
-    }
-  }, [
-    exportIsFetching,
-    exportIsLoading,
-    exportIsError,
-    exportIsSuccess,
-    exportData,
-  ]);
+  let sheetsUri = "";
+  if (isFetching || isLoading) buttonText = "Exporting...";
+  if (isError) {
+    console.log("error: ", error);
+    toast.error("An error occurred");
+    buttonText = "Retry";
+  }
+  if (isSuccess && data) {
+    toast.success("Successfully exported data");
+    console.log("data: ", data);
+    sheetsUri = data?.data?.data?.spreadsheetUrl;
+  }
 
   const exportToGoogleSheets = () => {
-    if (code && _gradingResponse?.length) {
-      const re = _gradingResponse?.map(({ score }, id) => [
+    if (code && gradingResponse_?.length) {
+      const re = gradingResponse_.map(({ score }, id) => [
         `${id + 1}`,
         `${score}`,
       ]);
       const header = ["Student ID", "Grade"];
       const sheetsObject = [header, ...re];
-      exportMutate({ code, sheetsObject }); // makes call to endpoint that (1) creates new sheet and (2) appends values to the sheet
+      console.log("sheetsObject: ", sheetsObject);
+      mutate({ code, sheetsObject }); // mutation that makes call to endpoint that (1) creates new sheet and (2) appends values to the sheet
     }
   };
 
-  // if (_gradingResponse[0]?.status == "rejected") {
-  //   return (
-  //     <div
-  //       style={{
-  //         width: "100%",
-  //         height: "100%",
-  //         display: "flex",
-  //         flexDirection: "column",
-  //         alignItems: "center",
-  //         justifyContent: "center",
-  //       }}
-  //     >
-  //       <p>Oops. An error occurred.</p>
-  //       <Button primary onClick={() => nav("/assessments")}>
-  //         Go back
-  //       </Button>
-  //     </div>
-  //   );
-  // }
-
-  console.log("sheetsUri: ", sheetsUri);
+  if (gradingResponse_[0]?.status == "rejected") {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p>Oops. An error occurred.</p>
+        <Button primary onClick={() => nav("/assessments")}>
+          Go back
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className="w-100 h-screen p-8 flex flex-col gap-6">
-      {sheetsUri && (
+      {Boolean(sheetsUri) && (
         <div className="w-full h-max flex flex-col gap-0.5 justify-end content-end text-end">
           <p>Google Sheets url:</p>
           <p
@@ -146,12 +138,11 @@ const Results = () => {
           </TableRow>
         </TableHeader>
 
-        {/* ?.map(({ value }) => JSON.parse(value)) */}
         <TableBody>
-          {_gradingResponse.map(({ score }, id) => (
+          {gradingResponse_?.map((el, id) => (
             <TableRow key={id}>
               <TableCell>{id + 1}</TableCell>
-              <TableCell>{score}</TableCell>
+              <TableCell>{el.score}</TableCell>
               <TableCell>
                 <Button secondary onClick={() => nav(`${id}`)}>
                   Review
