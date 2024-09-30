b021bfd628bd2ab0988fd67ccb0c8058cc10e707
Fiewor <johnfiewor@gmail.com>
2024-09-30 23:43:24 +0100

    refactor: display user data from localStorage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 const Header = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log("user: ", user);
+
   return (
-    <div className="bg-violet-50 w-100 h-[10dvh] p-6 border-b-neutral-600 flex justify-end items-center">
+    <div className="bg-violet-50 w-full h-[10dvh] p-6 border-b-neutral-600 flex justify-end items-center">
       <div className="flex items-center gap-2 p-4 justify-between">
         <span className="rounded-full width-[2rem] bg-neutral-800" />
 
-        <div className="flex flex-col items-end justify-between">
-          <p>John Abdulsalam Aboyeji</p>
-          <p>Lecturer</p>
+        <div className="flex gap-2">
+          <img src={user?.picture} alt="profile" />
+          <div className="flex flex-col items-end justify-between">
+            <p>{`${user?.first_name} ${user?.last_name}`}</p>
+          </div>
         </div>
       </div>
     </div>
