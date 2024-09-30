7950f808919d1c9219f7581e38b11cfc2b4580b6
Fiewor <johnfiewor@gmail.com>
2024-09-30 23:44:54 +0100

    feat: get user based with code, save user data in localStorage

--- src/components/Assessments.jsx
+++ src/components/Assessments.jsx
@@ -1,18 +1,46 @@
 import { useContext, useEffect } from "react";
 import SUIForm from "./SUIForm";
 import SUITable from "./SUITable";
-import { ModalContext } from "../pages/Home";
+import { ModalContext } from "../Layout";
 import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
 //
 const Assessments = () => {
   const { showModal } = useContext(ModalContext);
   const [searchParams] = useSearchParams();
 
+  const code = searchParams.get("code") || localStorage.getItem("code");
+
+  const { data, mutate } = useMutation({
+    mutationKey: ["profileData"],
+    mutationFn: async (code) => {
+      const res = await axios.get(`/getGoogleUser?code=${code}`);
+      const {
+        data: { data },
+      } = res;
+      localStorage.setItem("user: ", JSON.stringify(data));
+      return res;
+    },
+  });
+
   useEffect(() => {
-    if (searchParams.get("code")) {
-      localStorage.setItem("code", searchParams.get("code"));
+    if (code) {
+      mutate(code);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [code]);
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    if (code) {
+      localStorage.setItem("code", code);
     }
   }, [searchParams]);
+
+  useEffect(() => {
+    console.log("data: ", data);
+  }, [data]);
 
   return (
     <div className="w-100 p-4 flex flex-col justify-between gap-2 border-l-stone-950">
