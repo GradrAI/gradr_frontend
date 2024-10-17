059d523a9b73f63c69cf96c40a5eac0a16d9f65f
Fiewor <johnfiewor@gmail.com>
2024-10-17 15:00:08 +0100

    style(header): user name and image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 const Header = () => {
   const user = JSON.parse(localStorage.getItem("user"));
-  console.log("user: ", user);
 
   return (
     <div className="bg-violet-50 w-full h-[10dvh] p-6 border-b-neutral-600 flex justify-end items-center">
       <div className="flex items-center gap-2 p-4 justify-between">
         <span className="rounded-full width-[2rem] bg-neutral-800" />
 
-        <div className="flex gap-2">
-          <img src={user?.picture} alt="profile" />
+        <div className="flex items-center justify-between gap-4">
+          <img
+            src={user?.picture}
+            alt="profile"
+            className="w-[35px] rounded-full"
+          />
           <div className="flex flex-col items-end justify-between">
-            <p>{`${user?.first_name} ${user?.last_name}`}</p>
+            <p className="font-semibold">{`${user?.first_name} ${user?.last_name}`}</p>
           </div>
         </div>
       </div>
