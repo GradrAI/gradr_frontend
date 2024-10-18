7c282e38a9e61fa13d0a401dc90ba2b7a8846875
Fiewor <johnfiewor@gmail.com>
2024-10-18 23:12:40 +0100

    feat: add datatable +deps + related data

---
+++ src/components/columns.tsx
@@ -0,0 +1,79 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { DataTableColumnHeader } from "./DataTableColumnHeader";
+import { Exam } from "@/types/Exam";
+
+export const columns: ColumnDef<Partial<Exam>>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    accessorKey: "examName",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Exam Name" />
+    ),
+  },
+  {
+    id: "status",
+    accessorKey: "status",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Status" />
+    ),
+    cell: ({ row }) => {
+      return <p className="text-red-500">Ungraded</p>;
+    },
+  },
+  {
+    id: "createdAt",
+    accessorKey: "createdAt",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Created At" />
+    ),
+    cell: ({ row }) => {
+      const formattedDate = new Date(
+        row.getValue("createdAt")
+      ).toLocaleDateString("en-US");
+      return <div className="font-medium">{formattedDate}</div>;
+    },
+  },
+  //! TO-DO: think about actions to be added here
+  //   {
+  //     id: "actions",
+  //     header: "Actions",
+  //     cell: ({ row }) => {
+  //       const data = row.original;
+  //       console.log("row data: ", data);
+
+  //       return (
+  //         <DropdownMenu>
+  //           <DropdownMenuTrigger asChild>
+  //             <Button variant="ghost" className="h-8 w-8 p-0">
+  //               <span className="sr-only">Open menu</span>
+  //               <MoreHorizontal className="h-4 w-4" />
+  //             </Button>
+  //           </DropdownMenuTrigger>
+  //           <DropdownMenuContent align="end">
+  //             {/* <DropdownMenuItem>View customer</DropdownMenuItem> */}
+  //           </DropdownMenuContent>
+  //         </DropdownMenu>
+  //       );
+  //     },
+  //   },
+];
