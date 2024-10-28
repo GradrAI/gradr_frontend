50629657455f5b4171f888ed6bed1e2258b61700
Fiewor <johnfiewor@gmail.com>
2024-10-28 20:29:21 +0100

    refactor: remove select to make table view-only

--- src/pages/Assessments/columns.tsx
+++ src/pages/Assessments/columns.tsx
@@ -5,26 +5,6 @@ import { Exam } from "@/types/Exam";
 
 export const columns: ColumnDef<Partial<Exam>>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-  },
-  {
     accessorKey: "examName",
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Exam Name" />
