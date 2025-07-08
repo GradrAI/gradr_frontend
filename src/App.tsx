575e1eef886ba0d1cd6d230b7744a7b29bcd4db3
Fiewor John <johnfiewor@gmail.com>
2025-07-08 17:46:19 +0100

    remove lecturere protected route

--- src/App.tsx
+++ src/App.tsx
@@ -28,22 +28,6 @@ import AuthLayout from "./pages/Auth/AuthLayout";
 
 import "./App.css";
 import StudentLayout from "./pages/Student/components/StudentLayout";
-import { Navigate, Outlet } from "react-router-dom";
-
-// Route guard for lecturer-only routes
-function ProtectedLecturerRoute({
-  user,
-  children,
-}: {
-  user: any;
-  children?: React.ReactNode;
-}) {
-  if (!user || user.role === "student") {
-    // If not logged in or is a student, redirect to home
-    return <Navigate to="/" replace />;
-  }
-  return children ? children : <Outlet />;
-}
 
 function App() {
   const nav = useNavigate();
@@ -75,15 +59,7 @@ function App() {
           <Route path="confirmation" element={<PostPayment />} />
         </Route>
 
-        {/* Lecturer-only protected routes */}
-        <Route
-          path="app"
-          element={
-            <ProtectedLecturerRoute user={user}>
-              <Layout />
-            </ProtectedLecturerRoute>
-          }
-        >
+        <Route path="app" element={<Layout />}>
           <Route element={<Home />}>
             <Route index element={<Content />} />
           </Route>
