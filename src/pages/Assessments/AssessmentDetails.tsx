80597ddaa78235ad6ad6630bb3f6d0e0ec9950b3
Fiewor John <johnfiewor@gmail.com>
2026-03-09 22:48:49 +0100

    fix: assessment details

--- src/pages/Assessments/AssessmentDetails.tsx
+++ src/pages/Assessments/AssessmentDetails.tsx
@@ -1,6 +1,7 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import api from "@/lib/axios";
 import { Category } from "@/types/Category";
+import { Student } from "@/types/Student";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -24,37 +25,12 @@ const AssessmentDetails = () => {
       await api.get(
         `/courses/${decodeURI(courseId ?? "")}/students-by-category`
       ),
+      select: (res) => res.data.data,
   });
 
   useEffect(() => {
-    if (isSuccess && data?.data?.data) {
-      const uniqueStudentIds = new Set();
-      const scores: number[] = [];
-      let gradedCount = 0;
-
-      data?.data?.data.categories.forEach((category: Category) => {
-        category?.students?.forEach(({ student, result }) => {
-          uniqueStudentIds.add(student.studentId);
-          if (result?.score) {
-            gradedCount++;
-            const [score] = result.score.split("/").map(Number);
-            scores.push(score);
-          }
-        });
-      });
-
-      const totalScore = scores.reduce((sum, s) => sum + s, 0);
-      const averageScore = scores?.length
-        ? (totalScore / scores.length).toFixed(2)
-        : "0";
-
-      setAssessmentStat({
-        totalUniqueStudents: uniqueStudentIds.size,
-        gradedCount,
-        averageScore,
-        highestScore: scores.length ? Math.max(...scores) : 0,
-        lowestScore: scores.length ? Math.min(...scores) : 0,
-      });
+    if (isSuccess && data?.stats) {
+      setAssessmentStat(data.stats);
     }
   }, [isSuccess, data]);
 
@@ -77,32 +53,34 @@ const AssessmentDetails = () => {
       )}
       {data && (
         <>
-          <h1 className="text-green-500">{data?.data?.data?.courseName}</h1>
-          <p>
-            <span className="font-semibold text-xl">Categories:</span>{" "}
-            {data?.data?.data?.categories?.length}
-          </p>
+          <h1 className="text-green-500">{data?.courseName}</h1>
+          <div className="mt-4 flex flex-col gap-4">
+            <p>
+              <span className="font-semibold text-xl">Categories:</span>{" "}
+              {data?.categories?.length}
+            </p>
 
-          <p>
-            <span className="font-semibold text-xl">Number of students:</span>{" "}
-            <span className="">{assessmentStat?.totalUniqueStudents}</span>
-          </p>
-          <p>
-            <span className="font-semibold text-xl">Total Graded:</span>{" "}
-            <span className="">{assessmentStat?.gradedCount}</span>
-          </p>
-          <p>
-            <span className="font-semibold text-xl">Average Score:</span>{" "}
-            <span className="">{assessmentStat?.averageScore}</span>
-          </p>
-          <p>
-            <span className="font-semibold text-xl">Highest Score:</span>{" "}
-            <span className="">{assessmentStat?.highestScore}</span>
-          </p>
-          <p>
-            <span className="font-semibold text-xl">Lowest Score:</span>{" "}
-            <span className="">{assessmentStat?.lowestScore}</span>
-          </p>
+            <p>
+              <span className="font-semibold text-xl">Number of students:</span>{" "}
+              <span className="">{assessmentStat?.totalUniqueStudents}</span>
+            </p>
+            <p>
+              <span className="font-semibold text-xl">Total Graded:</span>{" "}
+              <span className="">{assessmentStat?.gradedCount}</span>
+            </p>
+            <p>
+              <span className="font-semibold text-xl">Average Score:</span>{" "}
+              <span className="">{assessmentStat?.averageScore}</span>
+            </p>
+            <p>
+              <span className="font-semibold text-xl">Highest Score:</span>{" "}
+              <span className="">{assessmentStat?.highestScore}</span>
+            </p>
+            <p>
+              <span className="font-semibold text-xl">Lowest Score:</span>{" "}
+              <span className="">{assessmentStat?.lowestScore}</span>
+            </p>
+          </div>
         </>
       )}
     </div>
